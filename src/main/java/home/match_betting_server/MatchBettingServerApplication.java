89f2cad416df686aba5cc1bd35907ab1bb09314d
Filip Kaminski <s95429@pollub.edu.pl>
2025-04-27 21:41:08 +0200

    Init commit

---
+++ src/main/java/home/match_betting_server/MatchBettingServerApplication.java
@@ -0,0 +1,13 @@
+package home.match_betting_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchBettingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchBettingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/match_betting_server/MatchBettingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package home.match_betting_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchBettingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
